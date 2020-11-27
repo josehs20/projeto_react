e2629f5661c7f8125b75649f8ee5c7adeedf1389
iLoir1n <47636215+iLoir1n@users.noreply.github.com>
2020-11-26 23:53:47 -0300

    Update index.js

--- src/pages/Meet/index.js
+++ src/pages/Meet/index.js
@@ -7,7 +7,63 @@ import meet from '../../meet.png'
 function Meet(){
   return(
       <div>
-        <h1>Jonas</h1>
+        <Container>
+                <Row>
+                    <Col sm="12" md={{ size: 9, offset: 3 }}><img src={meet} alt={"meet"}/>  </Col>
+                </Row>
+        </Container>
+
+        <div class="espacoTop"></div>
+       
+        <div className="container d-flex justify-content-center">
+          <h1>Como criar ou participar de uma aula?</h1>
+        </div>
+
+        <div className="container d-flex justify-content-center">
+          <ReactPlayer url="https://www.youtube.com/watch?v=Tc_YpQKMTtw"/>
+        </div>
+
+        <div class="espacoBottom"></div>
+
+        <div className="container d-flex justify-content-center">
+          <h1 class="filho">Como habilitar o microfone ou câmera?</h1>
+        </div>
+
+        <div className="container d-flex justify-content-center">
+          <ReactPlayer url="https://www.youtube.com/watch?v=kWOwQCE7xSo"/>
+        </div>
+
+        <div class="espacoBottom"></div>
+
+        <div className="container d-flex justify-content-center">
+          <h1>Como compartilhar a tela?</h1>
+        </div>
+
+        <div className="container d-flex justify-content-center">
+          <ReactPlayer url="https://www.youtube.com/watch?v=Fcwe_XPgiRo"/>
+        </div>
+
+        <div class="espacoBottom"></div>
+
+        <div className="container d-flex justify-content-center">
+          <h1>Como gravar a aula e postar no Google Classroom?</h1>
+        </div>
+
+        <div className="container d-flex justify-content-center">
+          <ReactPlayer url="https://www.youtube.com/watch?v=AR1GiY84C0k"/>
+        </div>
+
+        <div class="espacoBottom"></div>
+
+        <div class="footer"> 
+                <Row>
+                    <Col sm="12">
+                        <Card body>
+                            <Button color="secondary" href="https://meet.google.com/">Site do Google Meet</Button>
+                        </Card>
+                    </Col>
+                </Row>
+            </div>
       </div>
   );
 };
